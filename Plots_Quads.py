15bbcc2cf2a27dbb7ec92a89f192ec5078a2f19b
Simon Bolduc Beaudoin <Simon.Bolduc.Beaudoin@usherbrooke.ca>
2023-10-17 13:03:09 -0400

    Plot functions update

--- Plots_Quads.py
+++ Plots_Quads.py
@@ -31,10 +31,10 @@ def plot_betas(freq,betas,Labels):
     ax.set_ylabel(r'$\beta(f)$[~]')
     ax.set_xlim(0,12)
     fig.tight_layout()
-    fig.legend(loc='lower right',ncol=1, bbox_to_anchor=(0.965, 0.525),title = 'Mode',fontsize=12)
+    #fig.legend(loc='lower right',ncol=1, bbox_to_anchor=(0.965, 0.525),title = 'Mode',fontsize=12)
     return ax
 
-def plot_n_vs_I_DC(I,ns,P,freq_mins,freq_maxs,Labels,R,Te):
+def plot_n_vs_I_DC(I,ns,P,freq_mins,freq_maxs,Labels,R,Te,theorie=False):
     label_slice=slice(None)
     fig, ax = _subplots(1,1)
 
@@ -44,64 +44,68 @@ def plot_n_vs_I_DC(I,ns,P,freq_mins,freq_maxs,Labels,R,Te):
     B = -P[label_slice,1]
 
     for var,a,b,l,c_idx,f_min,f_max in zip( ns[...,0] , A  , B ,Labels[label_slice] ,color_idx, freq_mins,freq_maxs) :
-        ax.plot(I*1e6, (var )  ,marker='.',ls='None',markersize=7.,color=_cm.cool(c_idx),label=l)
-        ax.plot(I*1e6, (a*I-b)  ,ls='--',color=_cm.cool(c_idx))
-        ax.plot(I*1e6, n_theorie(I,f_min,f_max,R,Te),color=_cm.cool(c_idx),ls='-')
+        if theorie :
+            ax.plot(I*1e6, (var )  ,marker='.',ls='None',markersize=10.,color=_cm.cool(c_idx))
+            ax.plot(I*1e6, (a*I-b)  ,ls='--',color=_cm.cool(c_idx))
+            ax.plot(I*1e6, n_theorie(I,f_min,f_max,R,Te),color=_cm.cool(c_idx),ls='-',label=l)
+        else :
+            ax.plot(I*1e6, (var )  ,marker='.',ls='-',markersize=10.,color=_cm.cool(c_idx),label=l)
+            ax.plot(I*1e6, (a*I-b)  ,ls='--',color=_cm.cool(c_idx))
             
     #ax.set_ylim(0)
     ax.set_ylabel(r'$\langle n \rangle $ [~]')
     ax.set_xlabel(r'$I_{dc}[\mu A]$')
     fig.tight_layout()
     #fig.legend(loc='best', bbox_to_anchor=(1.15, 0.9))
-    fig.legend(loc='lower right',ncol=1, bbox_to_anchor=(0.95, 0.55),title = 'Mode',fontsize=12)
+    #fig.legend(loc='lower right',ncol=1, bbox_to_anchor=(0.95, 0.55),title = 'Mode',fontsize=12)
     return ax
         
 def plot_n_vs_I_AC(I,ns,Labels,label_slice=slice(None)):
     fig, ax = _subplots(1,1)
     color_idx = _np.linspace(0, 1.0, len(Labels[label_slice]))
     for var,l,c_idx in zip( ns[...,0] ,Labels[label_slice] ,color_idx) :
-        ax.plot(I*1e6, (var )  ,marker='.',ls='-',markersize=7.,color=_cm.cool(c_idx),label=l)   
+        ax.plot(I*1e6, (var )  ,marker='.',ls='-',markersize=10.,color=_cm.cool(c_idx),label=l)   
     ax.set_ylabel(r'$\langle n \rangle $ [~]')
-    ax.set_xlabel(r'$I_{ac}[\mu A rms]$')
+    ax.set_xlabel(r'$I_{ac}$[$\mu A$ rms]')
     fig.tight_layout()
-    fig.legend(loc='lower right',ncol=1, bbox_to_anchor=(0.95, 0.55),title = 'Mode',fontsize=12)
+    #fig.legend(loc='lower right',ncol=1, bbox_to_anchor=(0.95, 0.55),title = 'Mode',fontsize=12)
     return ax
     
 def plot_dn2_vs_I_DC(I,ns,Labels,label_slice=slice(None)):
     fig, ax = _subplots(1,1)
     color_idx = _np.linspace(0, 1.0, len(Labels[label_slice]))
     for var,l,c_idx in zip( ns[...,1] ,Labels[label_slice] ,color_idx) :
-        ax.plot(I*1e6, (var )  ,marker='.',ls='-',markersize=7.,color=_cm.cool(c_idx),label=l)
-    ax.set_ylabel(r'$\langle \delta n \rangle $ [~]')
+        ax.plot(I*1e6, (var )  ,marker='.',ls='-',markersize=10.,color=_cm.cool(c_idx),label=l)
+    ax.set_ylabel(r'$\langle \delta n^2 \rangle $ [~]')
     ax.set_xlabel(r'$I_{dc}[\mu A]$')
     fig.tight_layout()
-    fig.legend(loc='lower right',ncol=1, bbox_to_anchor=(0.95, 0.55),title = 'Mode',fontsize=12)
-    ax.grid()
+    #fig.legend(loc='lower right',ncol=1, bbox_to_anchor=(0.95, 0.55),title = 'Mode',fontsize=12)
+    #ax.grid()
     return ax
     
 def plot_dn2_vs_I_AC(I,ns,Labels,label_slice=slice(None)):
     fig, ax = _subplots(1,1)
     color_idx = _np.linspace(0, 1.0, len(Labels[label_slice]))
     for var,l,c_idx in zip( ns[...,1] ,Labels[label_slice] ,color_idx) :
-        ax.plot(I*1e6, (var )  ,marker='.',ls='-',markersize=7.,color=_cm.cool(c_idx),label=l)
-    ax.set_ylabel(r'$\langle \delta n \rangle $ [~]')
-    ax.set_xlabel(r'$I_{ac}[\mu A rms]$')
+        ax.plot(I*1e6, (var )  ,marker='.',ls='-',markersize=10.,color=_cm.cool(c_idx),label=l)
+    ax.set_ylabel(r'$\langle \delta n^2 \rangle $ [~]')
+    ax.set_xlabel(r'$I_{ac}$[$\mu A$ rms]')
     fig.tight_layout()
-    fig.legend(loc='lower right',ncol=1, bbox_to_anchor=(0.95, 0.55),title = 'Mode',fontsize=12)
-    ax.grid()
+    #fig.legend(loc='lower right',ncol=1, bbox_to_anchor=(0.95, 0.55),title = 'Mode',fontsize=12)
+    #ax.grid()
     return ax
     
 def plot_fano(ns,Labels,label_slice=slice(None),NNplusUn=True):
     fig, ax = _subplots(1,1)
     color_idx = _np.linspace(0, 1.0, len(Labels[label_slice]))
-    for n,dn2,l,c_idx in zip( ns[...,0],ns[...,1] , Labels[label_slice] ,color_idx) :
+    for n,dn2,l,c_idx in zip( ns[label_slice,...,0],ns[label_slice,...,1] , Labels[label_slice] ,color_idx) :
         ax.plot(n, (dn2)/(n)  ,marker='.',ls='-',markersize=15.,color=_cm.cool(c_idx),label=l) 
         if NNplusUn :
             ax.plot(n,(n+1),color='k')
     ax.set_ylim(1,3.0)
     ax.set_xlim(-0.1,2.0)
-    ax.set_ylabel(r'$\langle \delta n \rangle / \langle n \rangle$ [~]')
+    ax.set_ylabel(r'$\langle \delta n^2 \rangle / \langle n \rangle$ [~]')
     ax.set_xlabel(r'$\langle n\rangle $ [~]')
     fig.tight_layout()
-    fig.legend(loc='lower right',ncol=1, bbox_to_anchor=(0.95, 0.55),title = 'Mode',fontsize=12)
+    #fig.legend(loc='lower right',ncol=1, bbox_to_anchor=(0.95, 0.55),title = 'Mode',fontsize=12)
     return ax
