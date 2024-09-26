3d1c172cd32426b487706936e6ed53566feec40a
Simon Bolduc Beaudoin <Simon.Bolduc.Beaudoin@usherbrooke.ca>
2024-09-26 15:16:56 -0400

    (Minor) init update

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
 #!/bin/env/python
 #! -*- coding: utf-8 -*-
 
-__all__ = ["Methods","Routines","Scopes",]
+__all__ = ["Methods","Plots_Quads","Quadratures","Routines_Photoexcited","Routines_SII","Scopes","Scopes_Routines"]
